98199d2834194533e32dd7a3e0640dc1ed1340bd
softtscoder <kennethmei811@gmail.com>
2022-04-22 16:56:15 +0600

    res: added responsiveness in banner component
    Signed-off-by: IMZihad21 <imzihad@gmail.com>

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -4,60 +4,83 @@ import { Box, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import Vector010 from "@images/Vectors/Vector010.png";
 import Vector011 from "@images/Vectors/Vector011.png";
+{
+  /*
+   */
+}
 
 const Banner = () => {
   return (
-    <Grid
-      container
+    <Box
       component="section"
       sx={{
+        display: "flex",
+        flexDirection: "column",
         alignItems: "center",
-        justifyContent: "space-between",
+        justifyContent: "center",
         bgcolor: "champagne.main",
+        position: "relative",
       }}
     >
-      <Grid item xs={3} sx={{ display: "flex", alignItems: "center" }}>
+      <Box
+        sx={{
+          position: "absolute",
+          right: 0,
+          display: { xs: "none", md: "block" },
+        }}
+      >
+        <Image width="150px" height="215px" src={Vector011} alt="Image-alt" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 0,
+          display: { xs: "none", md: "block" },
+        }}
+      >
         <Image width="300px" height="400px" src={Vector010} alt="Image-alt" />
-      </Grid>
-      <Grid item xs={6} sx={{ textAlign: "center" }}>
-        <Typography
-          variant="h1"
-          component="h1"
-          sx={{ fontSize: "90px", fontWeight: "bold" }}
-        >
-          Your Daily
-        </Typography>
-        <Typography
-          variant="h1"
-          component="h1"
-          sx={{
-            fontSize: "90px",
-            fontWeight: "bold",
-            lineHeight: "0.7",
-            color: "vermillion.main",
-          }}
-        >
-          Podcast
-        </Typography>
-        <Typography sx={{ fontSize: "16px", fontWeight: "medium", my: "40px" }}>
-          We cover all kinds of categories and <br /> a weekly special guest.
-        </Typography>
-        <ShadowButton sx={{ height: "65px", width: "180px" }}>
-          SUBSCRIBE
-        </ShadowButton>
-      </Grid>
-      <Grid item xs={3}>
-        <Box sx={{ display: "flex", justifyContent: "end", px: "60px" }}>
-          <Image
-            layout="fixed"
-            width="150px"
-            height="215px"
-            src={Vector011}
-            alt="Image-alt"
-          />
-        </Box>
-      </Grid>
-    </Grid>
+      </Box>
+      <Typography
+        variant="h1"
+        component="h1"
+        sx={{
+          fontSize: { xs: "50px", md: "90px" },
+          fontWeight: "bold",
+          mt: "60px",
+          textAlign: "center",
+        }}
+      >
+        Your Daily
+      </Typography>
+      <Typography
+        variant="h1"
+        component="h1"
+        sx={{
+          fontSize: { xs: "50px", md: "90px" },
+          fontWeight: "bold",
+          lineHeight: "0.7",
+          color: "vermillion.main",
+          textAlign: "center",
+        }}
+      >
+        Podcast
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: "16px",
+          fontWeight: "medium",
+          my: "40px",
+          textAlign: "center",
+        }}
+      >
+        We cover all kinds of categories and <br /> a weekly special guest.
+      </Typography>
+      <ShadowButton
+        sx={{ height: "65px", width: "180px", mt: { xs: 0, md: "60px" } }}
+      >
+        SUBSCRIBE
+      </ShadowButton>
+    </Box>
   );
 };
 
