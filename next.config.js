6f03d3abbcbf90cb8828379e3e89eddc8b84fef8
softtscoder <kennethmei811@gmail.com>
2022-05-04 02:36:45 +0600

    conf: Add basepath for production
    Signed-off-by: ZèD <imzihad@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === "production" && "/podcast",
   images: {
-    domains: ["i.ibb.co"],
+    domains: [ "i.ibb.co" ],
   },
 };
