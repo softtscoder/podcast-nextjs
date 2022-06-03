e401e0cb790a74bb70b7a0f2ef76df7baa666038
softtscoder <kennethmei811@gmail.com>
2022-06-03 06:16:02 +0000

    Disable basepath in maain branch
    Signed-off-by: GitHub <noreply@github.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" && "/podcast",
+  // basePath: process.env.NODE_ENV === "production" && "/podcast",
   images: {
     domains: ["i.ibb.co"],
   },
